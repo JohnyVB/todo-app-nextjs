60b68edab76ee5a470a700cb737a5c101a70bcb1
Johny Villegas <johnyvgoog@gmail.com>
2022-10-16 14:53:09 -0500

    First commit

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FC } from "react";
+import { Box } from "@mui/material"
+import Head from "next/head";
+import { Navbar } from '../ui';
+
+interface Props {
+    title?: string;
+    children?: React.ReactNode
+}
+
+export const Layout: FC<Props> = ({ title = 'OpenTODO - app', children }) => {
+  return (
+    <Box sx={{ flexFlow: 1 }}>
+
+        {/* Title */}
+        <Head>{ title }</Head>
+
+        <Navbar />
+
+        {/* Sidebar */}
+
+        <Box sx={{ padding: '10px 20px' }}>
+            { children }
+        </Box>
+
+    </Box>
+  )
+}
