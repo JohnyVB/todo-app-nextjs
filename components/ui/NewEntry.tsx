50383e81d4077c4ac14716f465341d6424dad9fb
Johny Villegas <johnyvgoog@gmail.com>
2022-10-30 23:09:41 -0500

    Add New Entries Form

---
+++ components/ui/NewEntry.tsx
@@ -0,0 +1,43 @@
+import { Box, Button, TextField } from "@mui/material";
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+
+export const NewEntry = () => {
+  return (
+    <Box sx={{ marginBottom: 2, paddingX: 1 }}>
+        <Button
+            startIcon={ <AddCircleOutlineOutlinedIcon /> }
+            fullWidth
+            variant='outlined'
+        >
+            Agregar Nueva Tarea
+        </Button>
+        <TextField 
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1 }}
+            autoFocus
+            multiline
+            label='Nueva entrada'
+            helperText='Ingrese un valor'
+        />
+
+        <Box display='flex' justifyContent='space-between'>
+            <Button
+                variant="outlined"
+                endIcon={ <CancelOutlinedIcon /> }
+            >
+                Cancelar
+            </Button>
+
+            <Button
+                variant="outlined"
+                color="secondary"
+                endIcon={ <SaveOutlinedIcon /> }
+            >
+                Guardar
+            </Button>
+        </Box>
+    </Box>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Card, CardHeader, Grid } from '@mui/material';
 import type { NextPage } from 'next';
 import { Layout } from '../components/layouts';
-import { EntryList } from '../components/ui';
+import { EntryList, NewEntry } from '../components/ui';
 
 const HomePage: NextPage = () => {
   return (
@@ -13,6 +13,7 @@ const HomePage: NextPage = () => {
             <CardHeader title="Pendientes" />
             
             {/* Agregar una nueva entrada */}
+            <NewEntry />
             <EntryList status='pending' />
             
           </Card>
